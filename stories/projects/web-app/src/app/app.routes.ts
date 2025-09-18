8527be2d11935c6a65fd618baba2ecdfb02d3955
CharlyMaye <charlymayeux@hotmail.fr>
2025-09-18 11:28:22 +0000

    feat: implement home and material feature modules with routing and styling

--- stories/projects/web-app/src/app/app.routes.ts
+++ stories/projects/web-app/src/app/app.routes.ts
@@ -3,7 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'material',
-    loadChildren: () => import('./material').then((m) => m.provideFeatur({})),
+    loadChildren: () => import('./material').then((m) => m.provideFeature({})),
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home').then((m) => m.provideFeature({})),
   },
   {
     path: '',
--- stories/projects/web-app/src/app/material/index.ts
+++ stories/projects/web-app/src/app/material/index.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
-export function provideFeatur(options: {}) {
+export function provideFeature(options: {}) {
   const routes: Routes = [
     {
       path: '',
